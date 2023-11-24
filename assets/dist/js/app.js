3b2e517397709460ba0e62c3d5a2431e80edcfa8
Maede M. <mahavimaede@gmail.com>
2023-11-24 16:42:36 +0330

    Search Bar Css Html

---
+++ assets/dist/js/app.js
@@ -0,0 +1,7 @@
+let search = document.querySelector(".search");
+let searchBar = document.querySelector("#searchBar");
+function openCloseToggle() {
+  console.log("helllo");
+  search.classList.toggle("active");
+  searchBar.focus();
+}
